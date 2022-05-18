1ad8ac2b9e9bc3336f759f51c7121bd4a49dabe5
adamochi <honeyrumballs@gmail.com>
2022-05-18 16:03:29 +1000

    wellll

--- app.js
+++ app.js
@@ -6,3 +6,36 @@ function handleLightMode() {
 }
 
 lightModeBtn.addEventListener("click", handleLightMode);
+
+/*
+const lightModeBtn = document.getElementById("light-mode-button");
+const lightClass = document.getElementById("body");
+const modeInStorage = localStorage.getItem("light");
+
+
+
+function enableLight() {
+  body.classList.remove("light");
+    localStorage.setItem("light", "hello");
+  }
+
+function disableLight() {
+  body.classList.add("light");
+  localStorage.removeItem("light", null);
+}
+if (modeInStorage !== null) {
+  enableLight();
+}
+function handleTheLightBtn(){
+  if (modeInStorage === null) {
+    enableLight();
+    console.log("enabled");
+  } else {
+    disableLight();
+    console.log("disabled");
+
+  }
+}
+
+lightModeBtn.addEventListener("click", handleTheLightBtn);
+*/
